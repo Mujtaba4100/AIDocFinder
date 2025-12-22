95f8931d4dda42f4fcceec6d8fd750ded7afdbf6
Mujtaba4100 <saad444hassan@gmail.com>
2025-12-22 12:24:11 +0500

    remove extras

--- docufind_local/__init__.py
+++
@@ -1,6 +0,0 @@
-"""DocuFindLocal: 100% local desktop document search.
-
-This package is intentionally local-only and does not require a backend server.
-"""
-
-__all__ = ["ui", "local_search"]
--- docufind_local/ui/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["kivy_local_app"]
